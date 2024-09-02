8a7e34084468ac09e84b79d877c62404b07ac384
hhyufan <1838248655@qq.com>
2024-09-02 10:44:48 +0800

    feat：修改增加当前浇水影响时间的影响

--- src/main/kotlin/com/example/ThirdWateringTimeApp.kt
+++ src/main/kotlin/com/example/ThirdWateringTimeApp.kt
@@ -1,16 +1,21 @@
 package com.example
 import com.example.config.TimeConfig.MATURITY_WATER_RATIO
+import com.example.config.TimeConfig.earliestTime
+import com.example.config.TimeConfig.effectTime
 import com.example.config.TimeConfig.maturityTime
 import com.example.extensions.gapTo
 import com.example.extensions.plus
+import com.example.extensions.toDHHMMString
 import com.example.extensions.toFormattedString
 import com.example.utils.playSound
 import kotlinx.coroutines.*
 import org.example.com.example.config.AudioConfig.AUDIO_FILE_PATH
+import java.sql.SQLOutput
 import java.time.LocalDateTime
 import kotlin.time.Duration.Companion.minutes
-
-fun main() {
+// 12:12
+//
+fun thirdWateringTimeApp() {
     runBlocking {
         val initialNow = LocalDateTime.now()
         var stopTimeA: LocalDateTime = initialNow
@@ -18,17 +23,21 @@ fun main() {
         if (stopTimeA > stopTimeB) {
             return@runBlocking
         }
-
+        println(effectTime.toDHHMMString())
+        stopTimeB = stopTimeB.plus(-effectTime)
         while (stopTimeA < stopTimeB) {
             stopTimeA = stopTimeA.plus(1.minutes)
             stopTimeB = stopTimeB.plus((-1).minutes * MATURITY_WATER_RATIO)
         }
-
+        
         var stopTime = stopTimeA
         // 如果计算后的停止时间和成熟时间的差值小于32，则更新为当前时间 + 差值
-        if (initialNow gapTo maturityTime < 32.minutes) {
+        println(initialNow gapTo maturityTime)
+        println(earliestTime)
+        if (initialNow gapTo maturityTime < earliestTime) {
             stopTime = maturityTime
         }
+
 
         val job = launch(Dispatchers.Default) {
             while (isActive) {
@@ -43,4 +52,7 @@ fun main() {
         }
         job.join()
     }
+}
+fun main() {
+    thirdWateringTimeApp()
 }
